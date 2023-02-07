b43551d92f2188250669ce16ea306f3e8b820b83
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-02-07 10:22:38 -0500

    using of and range metods to create observables

--- src/create.js
+++ src/create.js
@@ -1,9 +1,26 @@
 import { displayLog } from './utils';
+import { of, range } from 'rxjs';
 
 export default () => {
-    /** start coding */
+    /** Of */
+    const source= of([1,2,3,4]);
+    const source2= of(
+        [1,2,3,4],
+        "Hello World",
+        {casa: "azul"},
+        function sayHello(){
+            return "Hello";
+        }
+    );
+    
+    const sus1= source.subscribe(data => displayLog(data));
+    displayLog("Second subscription");
+    const sus2= source2.subscribe(data => displayLog(data));
 
 
+    /** Range */
 
-    /** end coding */
+    const source3= range(2,10);
+    displayLog("Third subscription");
+    const sus3= source3.subscribe(data => displayLog(data))
 }
