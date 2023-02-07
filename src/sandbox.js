b8a9633a16a9b6843ed3e7f2e7567775a9b26c0d
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-02-07 11:59:55 -0500

    used only map since mapto it's deprecated

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,12 @@
 import { displayLog } from './utils';
+import { fromEvent } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
     const grid = document.getElementById('grid');
 
-
-    /** end coding */
+    fromEvent(grid, 'click').pipe(
+        map(val => [Math.floor(val.offsetX/50), Math.floor(val.offsetY/50)]),
+        filter(val=> (val[0]+val[1])%2!=0)
+        ).subscribe(data => displayLog(data));
 }
