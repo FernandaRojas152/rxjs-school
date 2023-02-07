4f7706afb9410207ceb735716c77edaf743827b6
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-02-07 16:57:35 -0500

    Update sandbox.js

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,17 @@
+import { fromEvent } from 'rxjs';
 import { updateDisplay, displayLog } from './utils';
+import { debounceTime, map } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     
     const inputBox = document.getElementById('input-box');
+    fromEvent(inputBox, "input")
+        .pipe(
+            debounceTime(500),
+            map(event => event.target.value),
+            )
+        .subscribe(displayLog);
 
     /** end coding */
 }
