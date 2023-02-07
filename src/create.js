54829b3a2d6d649bb4093bebb673bd1888a93ed3
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-02-07 10:54:16 -0500

    from event two ways of doing it implemented

--- src/create.js
+++ src/create.js
@@ -1,9 +1,10 @@
 import { displayLog } from './utils';
+import { from, fromEvent } from 'rxjs';
 
 export default () => {
-    /** start coding */
+    const actionBtn= document.querySelector("#action-btn");
+    const source = fromEvent(actionBtn, 'click');
+    source.subscribe(data => displayLog(`click event at position ${data.x} - ${data.y}`));
 
-
-
-    /** end coding */
+    fromEvent(actionBtn, 'mousemove').subscribe(data => console.log(data));
 }
