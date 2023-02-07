9565b0bbf6a4e26b07d776a08e2531c8512196da
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-02-06 23:55:18 -0500

    applying "from" method

--- src/create.js
+++ src/create.js
@@ -1,7 +1,21 @@
+import { from } from 'rxjs';
 import { displayLog } from './utils';
 
 export default () => {
     /** start coding */
+    const arr= [1,2,3,4,5,6];
+    const text="It's me, Hi! I'm the problem it's me";
+    //create a promise
+    const myPromise= new Promise(resolve=> {
+        setTimeout(() => {
+            resolve("It's me, Hi! I'm the problem it's me -Tay Tay");
+        }, 2000);
+    });
+
+
+    const myObservable= from(myPromise);
+
+    const subscriber= myObservable.subscribe(evt => displayLog(evt));
 
     /** end coding */
 }
