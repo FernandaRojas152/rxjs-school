04928c09551f1505f02b976dbc64616bb0cec7e7
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-02-07 10:42:07 -0500

    Update create.js

--- src/create.js
+++ src/create.js
@@ -1,9 +1,17 @@
 import { displayLog } from './utils';
+import { interval, timer } from 'rxjs';
 
 export default () => {
-    /** start coding */
+    const source= interval(500);
+    const sus= source.subscribe(data => displayLog(data));
 
+    timer(3000).subscribe(() => sus.unsubscribe()); // this is with a rxjs function 
+    // setTimeout(()=> sus.unsubscribe(), 3000); This is the way with a js vanilla function
 
+    const source2= timer(4000,100);
+    const sus2= source2.subscribe(data=> displayLog(`2-- ${data}`));
+
+    timer(6000).subscribe(()=> sus2.unsubscribe());
 
     /** end coding */
 }
