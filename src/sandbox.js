c5cf1414ee746e179561386d60b8a72bb471069c
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-02-08 12:08:11 -0500

    Update sandbox.js

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,17 +1,16 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
 import { fromEvent } from 'rxjs';
-import { map, scan, tap, mergeMap } from 'rxjs/operators';
+import { map, scan, tap, mergeMap, switchMap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
-    
     const button = document.getElementById('btn');
 
     /** get comments on button click */
     fromEvent(button, 'click').pipe(
         scan((acc, evt) => acc + 1, 0),            
-        mergeMap(id => api.getComment(id)),
+        switchMap(id => api.getComment(id)),
         map(JSON.stringify),
         tap(console.log),
     ).subscribe(displayLog);
